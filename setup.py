001ff6f80d49de992c24b78bb854a0f891b70aa6
rickstaa <rick.staa@outlook.com>
2019-01-09 00:12:30 +0100

    licence: Change licence clasifier

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
         },
         classifiers=[
             "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: GNU License",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
             "Operating System :: OS Independent",
         ],
         install_requires=[
