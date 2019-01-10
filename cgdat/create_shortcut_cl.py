274b7ce6bf6e8bd849a8318bab818bca95289321
rickstaa <rick.staa@outlook.com>
2019-01-10 01:47:22 +0100

    FIX: Adds working icon to shortcut and prevents two cmd prompts from opening

--- cgdat/create_shortcut_cl.py
+++ cgdat/create_shortcut_cl.py
@@ -3,6 +3,12 @@
 ### Import needed modules ###
 import os, winshell
 from win32com.client import Dispatch
+import sys
+
+### Import package modules ###
+import cgdat
+cgdat_gui_path = cgdat.__file__.replace('\\__init__.py','')
+python_path = sys.executable.replace('\\python.exe','\\pythonw.exe')
 
 ### Get relative script path ###
 DIRNAME = os.path.dirname(os.path.abspath(__file__))
@@ -10,7 +16,8 @@ DIRNAME = os.path.dirname(os.path.abspath(__file__))
 ### Set paths ###
 desktop = winshell.desktop()
 path = os.path.join(desktop, "CGDAT.lnk")
-target = r"cgdat-gui"
+target = python_path
+arguments = cgdat_gui_path + "\\cgdat_gui.py"
 icon_path = os.path.abspath(os.path.join(DIRNAME, "static/media/CGDAT.ico")).replace('\\','/')    # Toggle on icon
 
 ##############################################################
@@ -23,5 +30,6 @@ def main():
     shell = Dispatch('WScript.Shell')
     shortcut = shell.CreateShortCut(path)
     shortcut.Targetpath = target
+    shortcut.Arguments = arguments
     shortcut.IconLocation = icon_path
     shortcut.save()
